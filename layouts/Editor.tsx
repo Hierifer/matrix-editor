d32201c3af39808e46c80721e336b2cb46788cb6
Hierifer <hierifer@hotmail.com>
2024-09-17 01:38:48 +0800

    feat: add spine

--- layouts/Editor.tsx
+++ layouts/Editor.tsx
@@ -1,22 +1,22 @@
-import SplitPane from 'react-split-pane'
-import React from 'react'
+// import SplitPane from 'react-split-pane'
+// import React from 'react'
 
-// @ts-expect-error any
-const Editor = ({children}) => {
-    return (
-        //@ts-expect-error sp
-        <SplitPane
-            split="vertical"
-            minSize={50}
-            maxSize={300}
-            defaultSize={100}
-            style={{ background: '#f0f0f0', borderLeft: '1px solid #ccc' }}
-        >
-            <div style={{ background: 'red', height: '100%'}}></div>
-            <div style={{ background: 'blue', height: '100%' }}>{children}</div>
-            <div style={{ background: 'green', height: '100%' }}></div>
-        </SplitPane>
-    ) 
-}
+// // @ts-expect-error any
+// const Editor = ({children}) => {
+//     return (
+//         //@ts-expect-error sp
+//         <SplitPane
+//             split="vertical"
+//             minSize={50}
+//             maxSize={300}
+//             defaultSize={100}
+//             style={{ background: '#f0f0f0', borderLeft: '1px solid #ccc' }}
+//         >
+//             <div style={{ background: 'red', height: '100%'}}></div>
+//             <div style={{ background: 'blue', height: '100%' }}>{children}</div>
+//             <div style={{ background: 'green', height: '100%' }}></div>
+//         </SplitPane>
+//     ) 
+// }
 
-export default Editor
+// export default Editor
